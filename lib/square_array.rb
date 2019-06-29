7ad64baabb5b91f2a9a32bb45b1eed669e4e66f7
Janet Mendez <janetmendez.03@gmail.com>
2019-06-29 06:20:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   new_numbers = []
   
-  while count < array.size
+  while count < array.length
     num = array[count]
     new_numbers << (num ** num)
     count += 1
